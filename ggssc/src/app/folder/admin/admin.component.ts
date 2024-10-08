6855810d958a439ee253fe043cfe89961fa07602
Bhupinder Karan Singh <bhupinderkaransingh@gmail.com>
2024-10-07 20:30:21 -0400

    commit

--- ggssc/src/app/folder/admin/admin.component.ts
+++ ggssc/src/app/folder/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-admin',
@@ -8,10 +9,27 @@ import { Component, inject, OnInit } from '@angular/core';
 export class AdminComponent  implements OnInit {
   public title!: string;
   public page!: string;
-  constructor() {}
+  email!: string;
+  password!: string;
+  cpassword!:string;
+  success!:boolean;
+  name!: string;
+  constructor(
+    public toastr: ToastrService
+  ) {}
 
   ngOnInit() {
     this.title = "Guru Gobind Singh Study Circle, Canada";
     this.page = "Admin";
+  }
+
+  login(){
+    if((this.email!='' && this.password!='')&&(this.email!=null && this.password!=null)){
+      this.userService.SignIn(this.email, this.password);
+      this.email = ''; 
+      this.password = '';
+    }else{
+      this.toastr.error("Invalid credentials",'Message');
+    }
   }
 }
