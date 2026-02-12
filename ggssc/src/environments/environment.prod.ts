4381d000f8cec3b1c6fc87cae2bffce81d9c0149
webggssccanada <bhupinderkaransingh@gmail.com>
2026-02-12 13:44:23 -0500

    commit
    commit

--- ggssc/src/environments/environment.prod.ts
+++ ggssc/src/environments/environment.prod.ts
@@ -2,12 +2,6 @@ export const environment = {
   production: true,
   appsEndpoint:"https://api.bhupinderkaransingh.com/apps/api/v1",
     firebaseConfig : {
-    apiKey: "AIzaSyDuX4YUCYLDRb_UkO4tBHUQBlm8fZvniEc",
-    authDomain: "ggssc-349bf.firebaseapp.com",
-    projectId: "ggssc-349bf",
-    storageBucket: "ggssc-349bf.firebasestorage.app",
-    messagingSenderId: "475649081710",
-    appId: "1:475649081710:web:fc70326e3d3b978cc1417f",
-    measurementId: "G-V8L3HHZT3M"
+
   }
 };
--- ggssc/src/environments/environment.ts
+++ ggssc/src/environments/environment.ts
@@ -4,12 +4,6 @@ export const environment = {
   appsEndpoint:"http://localhost:8000/apps/api/v1",
   // appsEndpoint:"https://www.techatggssc.com:8443/apps/api/v1",
   firebaseConfig : {
-    apiKey: "AIzaSyDuX4YUCYLDRb_UkO4tBHUQBlm8fZvniEc",
-    authDomain: "ggssc-349bf.firebaseapp.com",
-    projectId: "ggssc-349bf",
-    storageBucket: "ggssc-349bf.firebasestorage.app",
-    messagingSenderId: "475649081710",
-    appId: "1:475649081710:web:fc70326e3d3b978cc1417f",
-    measurementId: "G-V8L3HHZT3M"
+
   }
 };
